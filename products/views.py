ef10bf527a3b9355abc83351a86010c031d22180
Aleksey170999 <alexbooster3@yandex.ru>
2022-06-24 12:29:43 +0300

    1. Added static, templates 2. Added navbar on site

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    age = 22
+    name = 'Aleksey'
+    return render(request, 'products/home_page.html', locals())
