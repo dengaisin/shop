1de7aa98f6e6024bee2797d7c1c7a0aa35de1725
Aleksey170999 <alexbooster3@yandex.ru>
2022-06-24 10:47:07 +0300

    Changed db to postgres

--- shop_prj/settings.py
+++ shop_prj/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'shop_prj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'aleksejtihonov',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
