b648c63f92f59fc16129c59b7779efb476b462ea
Aleksey170999 <alexbooster3@yandex.ru>
2022-06-25 22:54:37 +0300

    Test commit

--- products/urls.py
+++ products/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home_page, name='home-page'),
+    path('<int:id>', views.product_detail, nmae='product-detail'),
 ]
