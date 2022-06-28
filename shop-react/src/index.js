b63c14e1d206f6acdc1f78553569109f9a30cc33
Aleksey170999 <alexbooster3@yandex.ru>
2022-06-28 14:33:17 +0300

    Added registration for customers

---
+++ shop-react/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <div>
+        Привет!
+        <button>Ура!!!</button>
+    </div>,
+    document.getElementById('root')
+);
