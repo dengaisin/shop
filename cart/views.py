8391921cd3ea1e7e01bfe5baa862a8d5a432f688
dengaisin <106100274+dengaisin@users.noreply.github.com>
2022-06-29 14:21:24 +0500

    shopping cart is ready

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,11 @@ from products.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
+"""
+Это представление для добавления продуктов в
+корзину или обновления количества 
+для существующих продуктов
+"""
 
 
 @require_POST
@@ -16,19 +21,16 @@ def cart_add(request, product_id):
         cart.add(product=product,
                  quantity=cd['quantity'],
                  update_quantity=cd['update'])
-    return redirect('cart:cart_detail')
+    return redirect('cart_detail')
 
 
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
-    return redirect('cart:cart_detail')
+    return redirect('cart_detail')
 
 
 def cart_detail(request):
     cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
-                                                                   'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
